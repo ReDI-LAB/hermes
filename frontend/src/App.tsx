c0ff4979ff966460cff5e8bd794dcb58cf220231
ms84 <msindelicm@gmail.com>
2026-09-01 19:22:57 +0200

    chore: remove backend stuff from frontend branch: :triangular_flag_on_post:

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,10 +3,8 @@ function App() {
 
   return (
     <div className="flex min-h-screen items-center justify-center bg-slate-900">
-      <h1 className="text-4xl font-bold text-white" bground-color="bg-slate-900">
-        {/* Project Hermes Oktoberfest 2026 */}
-        My First Vercel Deployment! 🚀
-      <p>This deployed automatically from Git!</p>
+      <h1 className="text-4xl font-bold text-white">
+        Project Hermes
       </h1>
     </div>
   )
