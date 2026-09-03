f8e0d14471ab76917b8b54e2ab43488f802927d4
ms84 <msindelicm@gmail.com>
2026-09-03 09:27:24 +0200

    chore: deployment test: :rocket:

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,10 +4,11 @@ function App() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-slate-900">
       <h1 className="text-4xl font-bold text-white">
-        Project Hermes
+        Project Hermes<br />
+        <span className="text-2xl font-normal">Oktoberfest 2026 🍺</span>
       </h1>
     </div>
-  )
+  );
 }
 
 export default App
