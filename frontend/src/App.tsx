8dce398e72bdaae7588188fbbfd9d441da5a1af3
ms84 <msindelicm@gmail.com>
2026-09-09 19:34:48 +0200

    chore: deployment test#3: :rocket:

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,7 @@ function App() {
     <div className="flex min-h-screen items-center justify-center bg-slate-900">
       <h1 className="text-4xl font-bold text-white">
         Project Hermes<br />
-        <span className="text-2xl font-normal">Oktoberfest 2026 🍺</span>
+        <span className="text-2xl font-normal">Transparency tool for Oktoberfest 🍺</span>
       </h1>
     </div>
   );
